26ee5b4a74a9c74db2e90d25fa8fb4bc274b9d6c
SkyLord348 <70288178+SkyLord348@users.noreply.github.com>
2021-09-16 12:31:37 -0500

    Fixed spawn errors

--- Unit 2 Project/Assets/Challenge 2/Scripts/MoveForwardX.cs
+++ Unit 2 Project/Assets/Challenge 2/Scripts/MoveForwardX.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MoveForwardX : MonoBehaviour
 {
-    public float speed;
+    public float speed = 15f;
 
     // Update is called once per frame
     void Update()
